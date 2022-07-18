6b65af8679e382c44b65895857cbb2f10c8bb376
Dylan Black <dylan.black2021@gmail.com>
2022-07-18 17:31:14 -0400

    gallery

--- script.js
+++ script.js
@@ -119,5 +119,26 @@ contactForm.addEventListener('submit', (event) => {
       });
 });
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
-//
+// Next/previous controls
+
+const plusSlides = n => {
+  console.log("got here");
+  showSlides(slideIndex += n);
+}
+
+window.plusSlides = plusSlides;
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  console.log(slides);
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
